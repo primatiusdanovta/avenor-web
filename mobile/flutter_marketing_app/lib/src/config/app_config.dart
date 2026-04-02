a58bbad72b9fdee2d7efbc4c6a331388491fecb6
Primatius Danovta <primatiusdanovtakris@gmail.com>
2026-04-02 09:33:16 +0700

    config dart

--- mobile/flutter_marketing_app/lib/src/config/app_config.dart
+++ mobile/flutter_marketing_app/lib/src/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const apiBaseUrl = String.fromEnvironment(
     'AVENOR_API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000/api/mobile',
+    defaultValue: 'https://avenorperfume.site/api/mobile',
   );
 
   static const appName = 'Avenor Marketing';
